fa169720d3ded7fe90d17a9c8a1c00c768f1b9ef
Lucas <lucasfad@hotmail.com>
2023-05-22 23:45:04 +0800

    figure out how main will work

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
+from player import Player
+from user_interface import User_Interface
 import math
 
 print(math.sqrt(9))
 
+player_input = ""
+
+player = Player()
+is_alive = True
+
+while is_alive == False or player_input != "quit":
+    
+    player_input = input()
+    if player_input == 'dead':
+        is_alive = False
