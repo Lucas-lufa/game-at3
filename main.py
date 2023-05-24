47ef3888fb3728dbea44668c0640ea65a1f5be07
Lucas <lucasfad@hotmail.com>
2023-05-24 09:37:44 +0800

    play

--- main.py
+++ main.py
@@ -13,8 +13,24 @@ is_alive = True
 
 
 while player_input != "quit":   
-    
+    p1,i1 = ui.cutup_strings(10,
+                     world_map.map_tiles[world_map.player_position-8],
+                     80)
+    print(p1)
+    p1, i1 = ui.cutup_strings(10,
+                        world_map.map_tiles[world_map.player_position-1],
+                        19)
+    p2, i2 = ui.cutup_strings(10,
+                        world_map.map_tiles[world_map.player_position],
+                        38)
+    p3, i3 = ui.cutup_strings(10,
+                        world_map.map_tiles[world_map.player_position+1],
+                        19)
+    all_parts_print = "|"+p1+"|"+p2+"|"+p3+"|"
+    print(all_parts_print)
+    p1,i1 = ui.cutup_strings(10,
+                     world_map.map_tiles[world_map.player_position+8],
+                     80)
+    print(p1)
     player_input = input()
     world_map.navigation(player_input)
-
-
